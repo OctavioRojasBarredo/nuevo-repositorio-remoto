251a6a8bcd61048f631607bbc6e19d822b813da6
OctavioRojasBarredo <octaviorojasbarredo@gmail.com>
2023-12-04 09:20:04 +0100

    Nuevas líneas añadidas

--- App.java
+++ App.java
@@ -3,7 +3,9 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
-    System.out.println("Contenido 2b");
-  }
+    System.out.println("Contenido 2b"); 
+    System.out.println("Octavio");
+    System.out.println("San Andrés");
+}
   
 }
